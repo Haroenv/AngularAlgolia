0d00f4efa209e824cebe384b0616986356a140ba
Haroen Viaene <hello@haroen.me>
2017-03-25 13:47:06 +0100

    fix(setup) make instantsearch import work
    So for now the import works and the component gets initialized, but the widget doesn't work yet.
    
    I'm sorry that Angular + Algolia is still really hard right now, but we're working on solutions.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,75 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, Renderer, ElementRef } from '@angular/core';
 
-var instantsearch = require('instantsearch.js');
-//  wrong ? but sees the package ie 
-// ERROR in ./~/algoliasearch-helper/src/SearchParameters/index.js
-// var instantsearch = require('instantsearch.js');
-// const instantsearch = require('instantsearch.js');
+const instantsearch = require('instantsearch.js')
 
-
-
-// wrong: cant find the module ie
-//  ERROR in app.component.ts (10,31): Cannot find module 'instantsearch.js/dist/instantsearch.js'.)
-// import { instantsearch } from 'instantsearch.js/dist/instantsearch.js';
-// import { instantsearch } from 'instantsearch.js/src/lib/main.js';
-// import * as instantsearch from 'instantsearch.js';
-
-
-// wrong becuase it doesn't do anything
-// import { } from 'instantsearch.js';
-// import {  } from './../../node_modules/instantsearch.js/dist-es5-modulel/src/components/SearchBox';
-
-// wrong sytax ie
-//  Identifier expected.)
-// import { * } from './../../node_modules/instantsearch.js/';
-
-// wrong because instant search is not exported ie:
-//  Cannot find module 'instantsearch.js'.)
-// import instantsearch from 'instantsearch.js';
-
-// wrong because it can't be found , how do we use 'main' anyway?  
-// import { main } from 'instantsearch.js';
-
-
-// let search = instantsearch({
-//   appId: '$appId',
-//   apiKey: '$apiKey',
-//   indexName: '$indexName',
-//   urlSync: true
-// });
-//
-// let onRenderHandler = function() {};
-//
-// search.on('render', onRenderHandler);
-// // on renderHandler will be called
-// // until removeListener is called
-// search.removeListener(onRenderHandler);
-//
-// search.once('render', function(){  });
-// // triggered once then removed automatically
+let search = instantsearch({
+  appId: '$appId',
+  apiKey: '$apiKey',
+  indexName: '$indexName',
+  urlSync: true
+});
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+
+export class AppComponent implements AfterViewInit {
   title = 'app works!';
+  @ViewChild('searchbox') searchBox: ElementRef;
 
-
-  constructor() {
-    //     search.addWidget(
-    //   instantsearch.widgets.searchBox({
-    //     container: '#q',
-    //     placeholder: 'Search for products',
-    //     autofocus: false,
-    //     poweredBy: true
-    //   })
-    // );
-
-
+  constructor(private renderer: Renderer) {
   }
 
-  ngOnInit() {}
-
+  ngAfterViewInit() {
+    search.addWidget(
+      instantsearch.widgets.searchBox({
+        container: this.searchBox.nativeElement
+      })
+    );
+  }
 }
